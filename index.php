f7151a8d936c72a34b8ccbc2da018d0b5c5b194a
Umair Jibran <umairjibran7@gmail.com>
2020-07-14 00:34:49 +0500

    skeleton for message preview body

--- index.php
+++ index.php
@@ -9,6 +9,22 @@
 </head>
 <body>
    <h1 class="center">PHP Messenger</h1>
-   <p>Select a Conversation to continue or <a href="new.php" class="">Start a Conversation</a></p>
+   <div class="container container-fluid">
+      <p class="center">Select a Conversation to continue or <a href="new.php" class="">Start a Conversation</a></p>
+      <br>
+      <div class="message-body-preview row">
+         <aside class="user-photo col col-2">
+            <img src="https://via.placeholder.com/150" alt="">
+         </aside>
+         <aside class="message-preview col col-9">
+            <section class="message-sender">
+               <h3>John Doe</h3>
+            </section>
+            <section class="message">
+               <p>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet </p>
+            </section>
+         </aside>
+      </div>
+   </div>
 </body>
 </html>
