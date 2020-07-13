bb1743016ecdb9e33bd966718ed4025de39c9515
Umair Jibran <umairjibran7@gmail.com>
2020-07-14 01:41:15 +0500

    added server connection

---
+++ includes/connection.php
@@ -0,0 +1,13 @@
+<?php
+   $username = "root";
+   $password = "";
+   $server = "localhost";
+   $database = "php-messenger";
+
+   $connection = new mysqli($server, $username, $password, $database);
+   // Check connection
+   if ($connection->connect_error) {
+      die("Connection failed: " . $connection->connect_error);
+   }
+   echo "Connected successfully";
+?>
