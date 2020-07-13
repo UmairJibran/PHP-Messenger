50fca107ab0e79c89e30a2015fcf50c63aea5dea
Umair Jibran <umairjibran7@gmail.com>
2020-07-14 00:56:45 +0500

    added personalization

--- index.php
+++ index.php
@@ -10,19 +10,27 @@
 <body>
    <h1 class="center">PHP Messenger</h1>
    <div class="container container-fluid">
-      <p class="center">Select a Conversation to continue or <a href="new.php" class="">Start a Conversation</a></p>
+      <br>
+      <p class="center">
+         <small class="float-left">Welcome, Jane Doe!</small>
+         Select a Conversation to continue or <a href="new.php" class="">Start a Conversation</a>
+         <a class="float-right btn btn-outline-danger" href="logout.php">Log Out</a>
+      </p>
       <br>
       <div class="message-body-preview row">
-         <aside class="user-photo col col-2">
+         <aside class="user-photo col col-1 center">
             <img src="https://via.placeholder.com/150" alt="">
          </aside>
          <aside class="message-preview col col-9">
-            <section class="message-sender">
+            <section class="message-sender center">
                <h3>John Doe</h3>
             </section>
-            <section class="message">
+            <section class="message left">
                <p>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet </p>
             </section>
+         </aside>
+         <aside class="col col-2">
+            <a href="#" class="btn btn-outline-primary center">Chat with John</a>
          </aside>
       </div>
    </div>
