913e58ed28f9f26b1c8f97742404cb547198fe38
Umair Jibran <umairjibran7@gmail.com>
2020-07-14 00:17:44 +0500

    added master css

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/master.css">
+</head>
+<body>
+   <h1 class="center">PHP Messenger</h1>
+   <p>Select a Conversation to continue or <a href="new.php" class="">Start a Conversation</a></p>
+</body>
+</html>
