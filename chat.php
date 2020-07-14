acda7b16a3a62bf46cb2cd9d30c1c980d9743cc0
Umair Jibran <umairjibran7@gmail.com>
2020-07-14 16:35:32 +0500

    skeleton for chat page

--- chat.php
+++ chat.php
@@ -13,8 +13,22 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title><?php echo $recepientName?></title>
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/master.css">
 </head>
 <body>
-   
+   <div class="container container-fluid">
+      <h1 class="center pt-2 pb-2 header fixed-top">Chat with <?php echo $recepientName;?></h1>
+      <a href="./" class="float-left btn btn-outline-success">Home</a>
+      <a href="logout.php" class="float-right btn btn-outline-danger">Log Out</a>
+      <section class="three-quarters">
+      <div class="message-body recepient">
+            lol
+         </div>
+         <div class="message-body sender">
+            lol
+         </div>         
+      </section>
+   </div>
 </body>
 </html>
